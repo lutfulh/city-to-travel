b99f2689cf6761880afd6d53c4c249f53bd1d195
Lutful haider <lutfulhdr@gmail.com>
2019-02-07 21:33:45 +0200

    some modification

--- src/components/tourData.js
+++ src/components/tourData.js
@@ -5,7 +5,7 @@ export const tourData = [
     img: "./img/newyork.jpeg",
     name: "new york bridge tour",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "New York City boasts more spectacular bridges than any other city in the world. From the Gothic stone arches and gossamer steel webbing of the Brooklyn Bridge (perhaps the greatest engineering achievement of the 19th century), to the Verrazano-Narrows — the world's longest suspension bridge when completed in 1964 — more than 75 bridges span the city's waterways. This book is a stirring text-and-picture tribute to these awe-inspiring structures."
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const tourData = [
     img: "./img/paris.jpeg",
     name: "paris lights tour",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "The City of Love becomes the City of Lights.Famous for love stories, revolutions and royal intrigues, Paris and its iconic monuments stand as visible reminders of some of the most romantic periods of history. Your expert local guide will lead you through the streets of Paris after dark to see these incredible sites when they're all lit up."
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const tourData = [
     img: "./img/london.jpeg",
     name: "london royal palace tour",
     info:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+      "Royal London is a quarter steeped in tradition, where England’s Monarchs have resided for centuries. Our tour covers the historic buildings and places that still host the pageantry, ceremony and working life of the Royal Family.Starting at Buckingham Palace, the Monarch’s official London residence, we will describe the history and life of the Palace and, in summer months, visit the 19 magnificent State Rooms, viewing the Thrones and the Ballroom set for a State Banquet. Most days the unmissable spectacle of Changing of the Guard can also be enjoyed."
   },
   {
     id: 4,
